43197273ac8d6a2435566ee685783c9d115c38d2
Tech Deepak <141490582+techdeepak1996@users.noreply.github.com>
2025-01-27 11:43:20 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,14 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React from "react";
+
+// Convert App into a functional component
+const App = () => {
+  return (
+    <div id="main">
+      <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+    </div>
+  );
+};
+
+// Export App as default
+export default App;
 
